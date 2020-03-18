4c1caebfe97fe66f82ee48f3481f3013c56acbc0
TeddyBear2004 <engelmann44444@gmail.com>
2020-03-18 11:01:09 +0100

    updated Player Object

--- src/main/java/team/speckfamily/teddy/werewolf/data/Player.java
+++ src/main/java/team/speckfamily/teddy/werewolf/data/Player.java
@@ -1,14 +1,39 @@
 package team.speckfamily.teddy.werewolf.data;
 
 import net.dv8tion.jda.api.entities.User;
+import team.speckfamily.teddy.werewolf.game.PlayerType;
+
+import java.util.Objects;
 
 public class Player {
     private final User user;
-    public Player(User user){
+    private final PlayerType type;
+    public Player(User user, PlayerType type){
         this.user = user;
+        this.type = type;
+    }
+
+    public PlayerType getType() {
+        return type;
     }
 
     public User getUser() {
         return user;
+    }
+    public static Player of(User user){
+        return new Player(user, null);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(user, player.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user);
     }
 }
